451b5a88aba612ba0659f45abd41126a5eca9912
Mafia0331 <J.K.play2015@gmail.com>
2023-03-07 13:13:34 +0200

    FrontEnd

---
+++ js/script_login.js
@@ -0,0 +1,15 @@
+'use strict';
+// const submitButton = document.getElementById('submit');//нахожу кнопку
+// const logIn = document.getElementById('login');//нахожу введегнный логин
+// const pass = document.getElementById('password');//нахожу введенный пароль
+
+// submitButton.addEventListener("submit", (event) => { //отслеживаю событие нажатия на кнопку
+//     event.preventDefault();
+//     const logins = logIn.value; //беру значение в поле логина
+//     const passs = pass.value; //беру значение в поле пароля
+//     console.log(logins, passs); //вывожу в консоль
+// });
+
+
+const inputs = document.querySelector('.inputs');
+const logIn = inputs.fi
